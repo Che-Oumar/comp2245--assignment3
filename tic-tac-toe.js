58a05581dfcc956fee4105e2addab5f340e8ccd5
Che-Oumar Weston <cheoumar.weston@my.fiveislands.uwi.edu>
2025-10-19 19:19:52 -0400

    javascript 2

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,8 +1,17 @@
 
 document.addEventListener("DOMContentLoaded", function() {
-
-    const squares = document.querySelectorAll("#board div"); 
-    squares.forEach(function(square) {
-        square.classList.add("square");
-    });
+   const squares = document.querySelectorAll("#board div"); 
+   let turn = "X";
+   const state = Array(9).fill(null);
+   squares.forEach(function(square, index) {
+       square.classList.add("square");
+       square.addEventListener("click", function() {
+           if (state[index] === null) {
+               state[index] = turn;
+               square.classList.add(turn);
+               square.textContent = turn;
+               turn = turn === "X" ? "O" : "X";
+           }
+       });
+   });
 });
